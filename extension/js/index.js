dc5afef28ec2f21a0c462e603890858c0f95e870
Camilo Andres Salinas Martinez <iam@camilosalinas.me>
2019-09-28 14:59:35 -0500

    Jquery

--- extension/js/index.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-
-
-	chrome.tabs.executeScript(tab.ib, {
-		file:"https://code.jquery.com/jquery-3.4.1.min.js"
-	});
-});
--- extension/js/ui.js
+++
@@ -1,7 +0,0 @@
-$("head").append('<link href="' + chrome.extension.getURL('./ui/css/main.css') + '" rel="stylesheet">');
-
-
-// Append timeline
-$.get(chrome.extension.getURL('./ui/index.html'), function (data) {
-    $("body").append(data);
-});
